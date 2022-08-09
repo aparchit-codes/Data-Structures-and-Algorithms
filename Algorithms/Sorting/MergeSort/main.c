cc7544e4df00706f2cb5f4985518560b155015ab
aparchit-codes <109553171+aparchit-codes@users.noreply.github.com>
2022-08-09 15:07:08 +0530

    Update main.c

--- Algorithms/Sorting/MergeSort/main.c
+++ Algorithms/Sorting/MergeSort/main.c
@@ -2,30 +2,49 @@
 #include<stdlib.h>
 #include<math.h>
 
-merge(int A[], int p, int q, int r){
+merge(int A[], int const p, int const q, int const r){
     int n1 = q-p+1, n2 = r-q, i, j, k;
-    int *L=malloc((n1+1)*(sizeof(int))), *R=malloc((n2+1)*(sizeof(int)));
+
+    int *L=malloc((n1)*(sizeof(int))); 
+    int *R=malloc((n2)*(sizeof(int)));
+    
     for(i=0; i<n1; ++i)
-        L[i] = A[p+i-1];
-    printf("\n\nArray L[]:");
-    for(int i=0; i<n1; ++i)
-        printf("%d ", L[i]);
+        L[i] = A[p+i];
+    
     for(j=0; j<n2; ++j)
-        R[j] = A[q+j];
-    // L[n1+1]=INT_MAX, R[n2+1]=INT_MAX;
-    i=j=0;
-    for(k=p; k<=r; ++k){
-        if(L[i]<=R[j]){
-            A[k]=L[i];
-            i=i+1;
-        }else{
-            A[k]=R[j];
-            j=j+1;
+        R[j] = A[q+j+1];
+
+    i=0;j=0;k=p;
+    while(i<n1 && j<n2){
+        if(L[i] <= R[j]){
+            A[k] = L[i];
+            ++i;
+        } else{
+            A[k] = R[j];
+            ++j;
         }
+        ++k;
+    }
+
+    while(i<n1){
+        A[k] = L[i];
+        ++i;++k;
+    }
+    while(j<n2){
+        A[k] = R[j];
+        ++j;++k;
     }
 }
 
-mergeSort(int A[], int p, int r);
+mergeSort(int A[], int p, int r){
+
+    if(p<r){
+        int q=p+(r-p)/2;
+        mergeSort(A,p,q);
+        mergeSort(A, q+1, r);
+        merge(A, p, q, r);
+    }
+}
 
 int main(){
     int n;
@@ -34,21 +53,11 @@ int main(){
     for(int i=0; i<n; ++i)
         scanf("%d", &A[i]);
     mergeSort(A, 0, n-1);
+    printf("\n\n\nGiven Array: ");
+    for(int i=0; i<n; ++i)
+        printf("%d ", A[i]);
     printf("\n\n\nSorted Array: ");
     for(int i=0; i<n; ++i)
         printf("%d ", A[i]);
     return 0;
-}
-
-mergeSort(int A[], int p, int r){
-    // printf("\nvalue of p: %d", p);
-    // printf("\nvalue of r: %d", r);
-
-    if(p<r){
-        int q=floor((p+r)/2);
-        // printf("\nvalue of q: %d", q);
-        mergeSort(A,p,q);
-        mergeSort(A, q+1, r);
-        merge(A, p, q, r);
-    }
 }
